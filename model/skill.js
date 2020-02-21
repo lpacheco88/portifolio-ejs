c11de7059d866e07ab7b5051e473301ffcd2d6a7
luciano.rpa <luciano.pacheco@capgemini.com>
2020-02-21 18:01:32 -0300

    WIP 21/02.2

--- model/skill.js
+++ model/skill.js
@@ -1,18 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const skillSchema = new mongoose.Schema({
-	name             : {
-		type     : String,
-		required : true
-	},
-	workingWithSince : {
-		type     : Date,
-		required : true
-	},
-	knowHowLevel     : {
-		type     : Number,
-		required : true
-	}
+  name: {
+    type: String,
+    required: true
+  },
+  workingWithSince: {
+    type: Date,
+    required: true
+  },
+  skillImage: {
+    type: Buffer,
+    required: true
+  },
+  skillImageType: {
+    type: String,
+    required: true
+  },
+  knowHowLevel: {
+    type: Number,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('Skill', skillSchema);
+skillSchema.virtual("skillImagePath").get(function() {
+  if (this.skillImage != null && this.skillImageType != null) {
+    return `data:${
+      this.skillImageType
+    };charset=utf-8;base64,${this.skillImage.toString("base64")}`;
+  }
+});
+
+module.exports = mongoose.model("Skill", skillSchema);
