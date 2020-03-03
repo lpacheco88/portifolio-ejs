2103504492112eb1a5d4d0ca3a8cb3864526574a
luciano.rpa <luciano.pacheco@capgemini.com>
2020-03-03 18:13:56 -0300

    Heroku presentation

--- routes/contact.js
+++ routes/contact.js
@@ -1,14 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const nodeMailer = require("nodemailer");
 
+router.get("/", async (req, res) => {
+  try {
+    res.render("contacts/index");
+  } catch (error) {}
+});
 
-router.get('/', async (req, res) => {
-	
-	try {
-		res.render('contacts/index');
-	} catch (error) {
-	
-	}
+router.post("/formulario", async (req, res, next) => {
+  try {
+    let transporter = nodeMailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
+      auth: {
+        // should be replaced with real sender's account
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+    let mailOptions = {
+      // should be replaced with real recipient's account
+      to: req.body.email,
+      subject: "Contato do site" + req.body.nome,
+      body: req.body.mensagem
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Message %s sent: %s", info.messageId, info.response);
+    });
+    res.writeHead(301, { Location: "index.html" });
+    res.end();
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
